cacf4de5e140f00ae2fd96be383e1e8a143c518e
Sam K Varghese <ksam071@gmail.com>
2023-01-25 19:00:32 +0530

    UC3 Remove avoidable word

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is Hash table learning problem");
-        String sentence = " paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situation ";
+        String sentence = "paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situation";
         MyHashMap map1 = new MyHashMap();
         map1.frequencyCalc(sentence);
     }
--- src/MyHashMap.java
+++ src/MyHashMap.java
@@ -1,25 +1,25 @@
 import java.util.*;
 
 public class MyHashMap {
-
-
     void frequencyCalc(String str) {
         Map<String, Integer> map = new HashMap<>();
-
         String arr[] = str.split(" ");
-        for (int i = 0; i < arr.length; i++) {
 
+        for (int i = 0; i < arr.length; i++) {
             if (map.containsKey(arr[i])) {
                 map.put(arr[i], map.get(arr[i]) + 1);
             } else {
                 map.put(arr[i], 1);
             }
         }
+        System.out.println("Before removing avoidable: "+map);
 
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            System.out.println(entry.getKey() + " - " + entry.getValue());
+        for (int j = 0; j < arr.length; j++) {
+            if (map.containsKey("avoidable")) {
+                map.remove("avoidable");
+            }
+
         }
-
-
+        System.out.println("After removing avoidable: "+map);
     }
 }
