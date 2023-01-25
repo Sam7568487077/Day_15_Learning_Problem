621d5645f49ce1a6af39fbbdc0126ad500005557
Sam K Varghese <ksam071@gmail.com>
2023-01-25 17:16:17 +0530

    First push

---
+++ src/MyHashMap.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class MyHashMap {
+    void givenSentenceReturn()
+    {
+        String sentence = "To be or not to be";
+        Map<String,Integer> myMap = new HashMap<>();
+
+    }
+}
