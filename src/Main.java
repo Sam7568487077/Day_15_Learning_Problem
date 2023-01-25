c30b870638267d1f302f45e1018066147513d9d8
Sam K Varghese <ksam071@gmail.com>
2023-01-25 17:40:05 +0530

    Ability to find frequency

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is Hash table learning problem");
+        String sentence = "to be or not to be";
+        MyHashMap map1 = new MyHashMap();
+        map1.frequencyCalc(sentence);
     }
 }
--- src/MyHashMap.java
+++ src/MyHashMap.java
@@ -1,10 +1,26 @@
 import java.util.*;
+public class MyHashMap{
 
-public class MyHashMap {
-    void givenSentenceReturn()
+
+    void frequencyCalc(String str)
     {
-        String sentence = "To be or not to be";
-        Map<String,Integer> myMap = new HashMap<>();
+        Map<String,Integer> map = new HashMap<>();
+
+        String arr[] = str.split(" ");
+        for(int i =0;i<arr.length;i++) {
+
+            if (map.containsKey(arr[i])) {
+                map.put(arr[i], map.get(arr[i]) + 1);
+            } else {
+                map.put(arr[i], 1);
+            }
+        }
+
+            for(Map.Entry<String,Integer> entry: map.entrySet())
+            {
+                System.out.println(entry.getKey() + " - " +entry.getValue());
+            }
+
 
     }
 }
